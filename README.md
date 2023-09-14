2627af98f1a664019748805517b65d8f80811b47
Fernando del Pino <fherdelpino@hotmail.com>
2023-09-14 18:54:13 +1000

    Add swagger and README.md (#5)
    * add swagger and README.md
    
    * add proper description

---
+++ README.md
@@ -0,0 +1,21 @@
+# movie-tickets api
+
+A movie ticket price calculator by age and ticket type.
+
+## Build
+
+```bash
+./gradlew clean build
+```
+
+## Run
+
+```bash
+./gradlew bootRun
+```
+
+## Usage
+
+```
+http://localhost:8080/swagger-ui/index.html
+```
---
+++ src/main/java/com/example/movietickets/MovieTicketsConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.movietickets;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MovieTicketsConfiguration {
+    @Bean
+    public OpenAPI movieTicketsOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("movie-tickets API")
+                        .description("A movie ticket price calculator by age and ticket type.")
+                        .version("1.0"));
+    }
+}
