6e42ec1128894ae123bfc79be721e2c4610e9c14
Fernando del Pino <fherdelpino@hotmail.com>
2023-09-15 03:05:23 +1000

    add java doc

--- src/main/java/com/example/movietickets/service/config/MovieTicketTypeConfig.java
+++ src/main/java/com/example/movietickets/service/config/MovieTicketTypeConfig.java
@@ -13,6 +13,12 @@ public class MovieTicketTypeConfig implements Comparable<MovieTicketTypeConfig> 
     private Integer age;
     private MovieTicketDiscountConfig discount;
 
+    /**
+     * Method use by the Collection API to order the elements.
+     * It's intended to use decreasing order by age to simplify the algorithm that identifies the age range of a customer.
+     * @param anotherPrice the object to be compared.
+     * @return inverse behavior than Integer.compareTo() method.
+     */
     @Override
     public int compareTo(MovieTicketTypeConfig anotherPrice) {
         return anotherPrice.getAge().compareTo(this.getAge());
